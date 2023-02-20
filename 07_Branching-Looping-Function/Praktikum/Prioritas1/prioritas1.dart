aa4716cbebbb296229cf23e40726d9fc8273ebe0
Rizki Andika Setiadi <rizkiandika616@gmail.com>
2023-02-20 07:16:21 +0700

    Jawaban Section 7 belum selesai

---
+++ 07_Branching-Looping-Function/Praktikum/Prioritas1/prioritas1.dart
@@ -0,0 +1,27 @@
+void main(List<String> args) {
+  // RIZKI ANDIKA SETIADI
+  // PRIORITAS 1
+  // NO 1
+  print("===============");
+  print("NO 1");
+  int nilai = 95;
+  print("Nilainya adalah : $nilai");
+  if(nilai > 70){
+    print("Nilai A");
+  }else if(nilai > 40){
+    print("Nilai B");
+  }else if(nilai > 0){
+    print("Nilai C");
+  }else{
+    print("");
+  }
+  print("===============");
+
+  // NO 2
+  // PERULANGAN
+  print("NO 2");
+  for(int i = 1; i <= 10; i++){
+    print(i);
+  }
+  print("===============");
+}
