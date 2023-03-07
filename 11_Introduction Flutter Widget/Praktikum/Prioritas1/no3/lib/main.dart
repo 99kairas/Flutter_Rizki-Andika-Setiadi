278422ee8598e79f1c06b84d896a3d0b5992e6a8
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-03-07 15:20:37 +0700

    Menambahkan jawaban Prioritas 1 dan 2

--- 11_Introduction Flutter Widget/Praktikum/Prioritas1/no3/lib/main.dart
+++ 11_Introduction Flutter Widget/Praktikum/Prioritas1/no3/lib/main.dart
@@ -9,11 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Alterra Academy',
+      theme: ThemeData.dark(),
+      home: const MyHomePage(title: 'Halo Alterra'),
     );
   }
 }
@@ -28,38 +27,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Halo Alterra Academy'),
+        centerTitle: true,
+        
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: const <Widget>[
+             Text(
+              'Nama saya adalah Rizki Andika Setiadi\ndari kelas Flutter C',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 20.0,
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
