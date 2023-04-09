432587249be384f3032365f25e601acecb32025c
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-04-09 07:24:09 +0700

    Modifikasi jawaban praktikum REST API - JSON

--- 25_Introduction REST API - JSON/Praktikum/project/lib/soal_section17/pages/create_page.dart
+++ 25_Introduction REST API - JSON/Praktikum/project/lib/soal_section17/pages/create_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:project/soal_section17/services/contact_service.dart';
+import 'package:project/soal_section17/services/image_service.dart';
 
 class CreateContactWithApiPage extends StatefulWidget {
   const CreateContactWithApiPage({super.key});
@@ -23,6 +26,11 @@ class _CreateContactWithApiPageState extends State<CreateContactWithApiPage> {
   String _errorPhoneMessage = '';
   bool _isButtonPhoneValid = false;
   // END OF PHONE PROPERTY
+
+  // IMAGE PROPERTY
+  String image = '';
+  // END OF IMAGE PROPERTY
+
   bool isLoading = false;
 
   @override
@@ -32,70 +40,104 @@ class _CreateContactWithApiPageState extends State<CreateContactWithApiPage> {
         title: const Text('Create Contact'),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              TextFormField(
-                onChanged: (value) {
-                  _username = value;
-                  if (_username.isEmpty) {
-                    _isUsernameValid = false;
-                    _isButtonNameValid = false;
-                    _errorUsernameMessage = 'Username tidak boleh kosong!';
-                  } else if (_username.length < 4) {
-                    _isUsernameValid = false;
-                    _isButtonNameValid = false;
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                image.isNotEmpty
+                    ? SvgPicture.string(
+                        image,
+                        height: 200,
+                        width: 200,
+                      )
+                    : Text('Image belum ada'),
+                const SizedBox(height: 10),
+                TextFormField(
+                  onChanged: (value) {
+                    _username = value;
+                    if (_username.isEmpty) {
+                      _isUsernameValid = false;
+                      _isButtonNameValid = false;
+                      _errorUsernameMessage = 'Username tidak boleh kosong!';
+                    } else if (_username.length < 4) {
+                      _isUsernameValid = false;
+                      _isButtonNameValid = false;
 
-                    _errorUsernameMessage = "Username harus lebih dari 4 Huruf";
-                  } else {
-                    _isButtonNameValid = true;
-                    _isUsernameValid = true;
-                  }
-                  setState(() {});
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Nama',
-                  hintText: 'Masukkan Nama',
-                  border: OutlineInputBorder(),
+                      _errorUsernameMessage =
+                          "Username harus lebih dari 4 Huruf";
+                    } else {
+                      _isButtonNameValid = true;
+                      _isUsernameValid = true;
+                    }
+                    setState(() {});
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Nama',
+                    hintText: 'Masukkan Nama',
+                    errorText: _isUsernameValid ? null : _errorUsernameMessage,
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                onChanged: (value) {
-                  _phone = value;
-                  if (_phone.isEmpty) {
-                    _isPhoneValid = false;
-                    _isButtonPhoneValid = false;
-                    _errorPhoneMessage = 'Nomor tidak boleh kosong!';
-                  }
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Nomor Handphone',
-                  hintText: 'Masukkan Nomor Handphone',
-                  border: OutlineInputBorder(),
+                const SizedBox(height: 10),
+                TextFormField(
+                  keyboardType: TextInputType.phone,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                  ],
+                  onChanged: (value) {
+                    _phone = value;
+                    if (_phone.isEmpty) {
+                      _isPhoneValid = false;
+                      _isButtonPhoneValid = false;
+                      _errorPhoneMessage = 'Nomor tidak boleh kosong!';
+                    } else if(_phone.length < 5){
+                       _isPhoneValid = false;
+                      _isButtonPhoneValid = false;
+                      _errorPhoneMessage = 'Nomor harus lebih dari 5 angka!';
+                    }
+                    else {
+                      _isPhoneValid = true;
+                      _isButtonPhoneValid = true;
+                    }
+                    setState(() {});
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Nomor Handphone',
+                    hintText: 'Masukkan Nomor Handphone',
+                    errorText: _isPhoneValid ? null : _errorPhoneMessage,
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              isLoading
-                  ? const CircularProgressIndicator()
-                  : ElevatedButton(
-                      onPressed: () async {
-                        isLoading = true;
-                        setState(() {});
-                        await ContactService().postContact(
-                          context,
-                          name: _username,
-                          phone: _phone,
-                        );
-                        Future.delayed(const Duration(milliseconds: 300), () {
-                          isLoading = false;
-                          setState(() {});
-                        });
-                      },
-                      child: const Text('Create Contact'),
-                    ),
-            ],
+                const SizedBox(height: 30),
+                isLoading
+                    ? const CircularProgressIndicator()
+                    : ElevatedButton(
+                        onPressed: _isButtonNameValid && _isButtonPhoneValid
+                            ? () async {
+                                isLoading = true;
+                                setState(() {});
+                                await ContactService().postContact(
+                                  context,
+                                  name: _username,
+                                  phone: _phone,
+                                );
+                                Future.delayed(
+                                    const Duration(milliseconds: 300), () {
+                                  isLoading = false;
+                                  setState(() {});
+                                });
+                                if (_username.isNotEmpty) {
+                                  image = await ImageService()
+                                      .generateImage(_username);
+                                  setState(() {});
+                                }
+                              }
+                            : null,
+                        child: const Text('Create Contact and Generate Image'),
+                      ),
+              ],
+            ),
           ),
         ),
       ),
